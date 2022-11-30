af67e3481bc151d7eece7fc6e7d1b499c42f8afc
SrVitinho <55586232+SrVitinho@users.noreply.github.com>
2022-11-30 03:30:10 -0300

    Parcial code correction: Changes: -> Thread fix FIXED! -> Menu Insertion (On going -> Graph FIXED | -> Watchers menu FIXED) -> Watchers created
    Pending:
    -> Database integration
    -> Menu Insertion (On going -> Pending Database integration)

--- Classes/basic_item.py
+++ Classes/basic_item.py
@@ -3,12 +3,12 @@ from datetime import datetime
 
 class BasicItem:
 
-    price = []
-    time = []
 
     def __init__(self, dada_raw, name):
         summary = dada_raw['products'][name]['buy_summary']
         self.id_name = dada_raw['products'][name]['product_id']
+        self.price = []
+        self.time = []
         if type(summary) is list:  # Caso nao possua muitas ofertas de compra não será lista
             self.name = name
             self.price.append(dada_raw['products'][name]['buy_summary'][0]['pricePerUnit'])
--- Main1.py
+++ Main1.py
@@ -5,6 +5,9 @@ from Classes.especial_item import EspecialItem
 import matplotlib.pyplot as plt
 import time
 import threading as tdr
+from datetime import datetime
+
+watchers = {}
 
 
 def getsjson():
@@ -16,61 +19,64 @@ def getsjson():
 
 def graph(especial_itens, itens, id, Case):
     if Case:
-        for i in itens:
-            if i.id_name == id:
-                item = i
+        item = itens[id]
     else:
-        for i in especial_itens:
-            if i.id_name == id:
-                item = i
+        especial_itens[id]
 
-    x = item.price
-    y = item.time
+    y = item.price
+    x = item.time
     plt.plot(x, y)
     plt.ylabel('Preco')
     plt.xlabel('Tempo')
     plt.show()
 
 
-def getdata(i_name, e_name, itens): #needs correction
-    rc = getsjson()
-    for i in i_name:
-        summary = rc['products'][i_name]['buy_summary']
-        if type(summary) is list:
-             i_price = rc['products'][i_name]['buy_summary'][0]['pricePerUnit']
-        else:
-            i_price = rc['products'][i_name]['buy_summary']['pricePerUnit']
+def getdata(i_name, e_name, itens, e_itens):
+    while True:
+        print('New circle started')
+        rc = getsjson()
 
+        for i in i_name:
+            summary = rc['products'][i]['buy_summary']
+            if type(summary) is list:
+                i_price = rc['products'][i]['buy_summary'][0]['pricePerUnit']
+                itens[i].price.append(i_price)
+                itens[i].time.append(datetime.now().strftime("%H:%M:%S"))
+            else:
+                i_price = rc['products'][i]['buy_summary']['pricePerUnit']
+                itens[i].price.append(i_price)
+                itens[i].time.append(datetime.now().strftime("%H:%M:%S"))
 
-
-    for i in e_name:
-        summary = rc['products'][e_name]['buy_summary']
-        if type(summary) is list:
-           e_price = rc['products'][e_name]['buy_summary'][0]['pricePerUnit']
-        else:
-           e_price = rc['products'][e_name]['buy_summary']['pricePerUnit']
-
-
-
-
+        for i in e_name:
+            summary = rc['products'][i]['buy_summary']
+            if type(summary) is list:
+                e_price = rc['products'][i]['buy_summary'][0]['pricePerUnit']
+                e_itens[i].price.append(e_price)
+                e_itens[i].time.append(datetime.now().strftime("%H:%M:%S"))
+            else:
+                e_price = rc['products'][i]['buy_summary']['pricePerUnit']
+                e_itens[i].price.append(e_price)
+                e_itens[i].time.append(datetime.now().strftime("%H:%M:%S"))
+        checkwatcher(itens)
+        time.sleep(5)
 
 
 def itensregistration():
     rc = getsjson()
     names = (rc['products']).keys()
-    itens = []
-    especial_itens = []
+    itens = {}
+    especial_itens = {}
     itens_names = []
     especial_names = []
     error_normal = 0
     error_especial = 0
 
-    for i in names: # loop adição dos itens
+    for i in names:  # loop adição dos itens
         if not i.startswith('ENCHANTED_'):
             try:  # try - except para itens nao disponiveis
                 print('trying to add ' + i)
-                itens.append(BasicItem(rc, i))
-                itens_names = i
+                itens[i] = BasicItem(rc, i)
+                itens_names.append(i)
                 print('Added')
             except Exception as err:
                 print(type(err).__name__)
@@ -80,8 +86,8 @@ def itensregistration():
         if i.startswith('ENCHANTED_'):
             try:
                 print('trying to add ' + i)
-                especial_itens.append(EspecialItem(rc, i))
-                especial_names
+                especial_itens[i] = EspecialItem(rc, i)
+                especial_names.append(i)
                 print('Added')
             except Exception as err:
                 print(type(err).__name__)
@@ -92,18 +98,78 @@ def itensregistration():
     print("Final error_especial count:" + error_especial.__str__())
     return especial_itens, itens, itens_names, especial_names
 
-    # Sistema pra selecionar o item -> entrada de usuario
 
-    # Watcher -> alarme para avisar de preço -> discord
+def menuwatchers():
+    print("1 -> Adicionar Watcher")
+    print("2 -> Listar Watchers")
+    print("3 -> Remover Watchers")
+
+    user_input = int(input())
+
+    if user_input == 1:
+        id = input("Qual o id do item?")
+        price_target = float(input("Qual o valor alvo?"))
+
+        watchers = addwatcher(id, price_target)
+
+    elif user_input == 2:
+        listwatcher()
+
+    elif user_input == 3:
+        id_to_remove = input("Qual o id do item a ser removido?")
+
+        watchers = removewatcher(id_to_remove)
 
 
+def checkwatcher(itens):
+    watchers_keys = watchers.keys()
+    for i in watchers_keys:
+        if watchers[i] <= itens[i].price[-1]:
+            print('ALARME NO WATCHER: ' + i)
 
+
+def removewatcher(id):
+    try:
+        del watchers[id]
+    except Exception as err:
+        print('Item nao cadastrado dentro dos watchers')
+
+
+def addwatcher(id, price_target):
+    try:
+        watchers[id] = price_target
+    except Exception as err:
+        print('Item ja cadastrado')
+
+
+def listwatcher():
+    for i in watchers:
+        print(i + " com price target de " + str(watchers[i]))
 
 
 if __name__ == "__main__":
-    especial_itens, itens = itensregistration()
-    time.sleep(5)
-    graph(especial_itens, itens, 'INK_SACK:4', 1)
+    especial_itens, itens, itens_name, especial_itens_names = itensregistration()
+
+    x = tdr.Thread(target=getdata, args=(itens_name, especial_itens_names, itens, especial_itens))
+    x.start()
+
+    #  graph(especial_itens, itens, 'INK_SACK:4', 1)
+
     while True:
-
-
+        print("Qual operacao gostaria de realizar?")
+        print("1 -> Verificar um grafico")
+        print("2 -> Menu Watcher")
+        print("3 -> Cadastrar Itens (ATENCAO ESSA OPERACAO RESETA TODAS AS TABELAS e seus valores!)")
+        decision = int(input())
+        print(decision)
+        if decision == 1:
+            print("Insira o ID do produto")
+            name = input()
+            if name.startswith('ENCHANTED_'):
+                graph(especial_itens, itens, name, 0)
+            else:
+                graph(especial_itens, itens, name, 1)
+        elif decision == 2:
+            menuwatchers()
+        else:
+            print('Entrada Invalida')
